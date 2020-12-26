f924b6c9735a43d0f5d7b3216f4485a7dc733dd5
umeranjum17 <umeranjum17@gmail.com>
2020-12-26 22:53:29 +0400

    Update server.js

--- server.js
+++ server.js
@@ -6,16 +6,16 @@ class User extends Model { }
 User.init({
     username: DataTypes.STRING,
     birthday: DataTypes.DATE
-}, {  sequelize, modelName: 'user' });
+}, { sequelize, modelName: 'user' });
 
 (async () => {
-   let myUser= await  User.schema('zeeyarah').sync()
-   
-   let buildAtrip= await  User.schema('buildatrip').sync()
+    let myUser = await User.schema('zeeyarah').sync()
+
+    let buildAtrip = await User.schema('buildatrip').sync()
     // let data = await User.findAll();
     // console.log(data)
-console.log(await myUser.findAll())
-console.log(await buildAtrip.findAll())
+    console.log(await myUser.findAll())
+    console.log(await buildAtrip.findAll())
     //   await sequelize.sync({force:true});
     //   const jane = await myUser.create({
     //     username: 'zeeyarah',
